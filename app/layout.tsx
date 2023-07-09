601f5a44e14fc12ffa5375ef57500b1b8c8f39b3
Md. Arshad <arshadchowdhury46@gmail.com>
2023-07-09 21:17:00 +0600

    designed background color and navbar at top

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,36 @@
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 
-import './globals.css';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Online Store by Arshad',
-  description: 'This is a practice online store project',
-}
+  title: "Online Store by Arshad",
+  description: "This is a practice online store project",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <Header />
-      <section className="min-h-screen max-w-7xl mx-auto">{children}</section>
-      <Footer />
+      <body
+        className={
+          inter.className &&
+          "bg-gradient-to-tl from-slate-900 via-slate-400 to to-slate-100"
+        }
+      >
+        <Header />
+        <section className="min-h-screen max-w-7xl mx-auto">{children}</section>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,31 +1,35 @@
 import Link from "next/link";
 
 const Header = () => {
+
+const hoverStyle = {
+  hover : "hover:text-gray-50 hover:border-b-2 hover:border-gray-500"
+}
+
   return (
-    <nav className="flex justify-around my-4">
+    <nav className="flex justify-around items-center my-2 text-gray-900">
       <div>
         <Link href={"/"}>Logo</Link>
       </div>
-
-      <div className="flex gap-8 justify-between">
-        <Link className="hover:opacity-50" href={"/"}>
+      <div className="flex justify-between items-center gap-8 text-lg">
+        <Link className={hoverStyle.hover} href={"/"}>
           Home
         </Link>
-        <Link className="hover:opacity-50" href={"/about"}>
+        <Link className={hoverStyle.hover} href={"/about"}>
           About
         </Link>
-        <Link className="hover:opacity-50" href={"/categories"}>
+        <Link className={hoverStyle.hover} href={"/categories"}>
           Categories
         </Link>
-        <Link className="hover:opacity-50" href={"/services"}>
+        <Link className={hoverStyle.hover} href={"/services"}>
           Services
         </Link>
-        <Link className="hover:opacity-50" href={"/contact"}>
+        <Link className={hoverStyle.hover} href={"/contact"}>
           Contact
         </Link>
-        <Link className="mx-8 hover:opacity-50" href={"/sign-up"}>
-          Sign Up
-        </Link>
+          <Link className="bg-slate-100 px-4 py-2 rounded-md hover:text-gray-50 hover:bg-slate-500" href={"/sign-up"}>
+            Sign Up / Login
+          </Link>
       </div>
     </nav>
   );
