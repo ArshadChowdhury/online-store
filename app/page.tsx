c978f8d32acd8607a9343a4ce7a49142e3b22023
Md. Arshad <arshadchowdhury46@gmail.com>
2024-01-13 15:47:58 +0600

    Added photos and setup swiper

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,114 @@
+"use client";
 
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import "swiper/css";
 
 export default function Home() {
   return (
     <>
-      <main className="">
-        <h1>Hey</h1>
+      <main className="max-w-3xl mx-auto my-10 aspect-[16/9] object-contain">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide className="relative">
+            <Image
+              className="rounded-md max-w-full h-auto"
+              src={"/image-1.jpg"}
+              style={{objectFit: "cover"}}
+              quality={100}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image
+              className="rounded-md max-w-full h-auto "
+              src={"/image-2.jpg"}
+              // style={{ maxWidth: "100%", height: "auto" }}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image
+              className="rounded-md"
+              src={"/image-3.avif"}
+              // style={{ maxWidth: "100%", height: "auto" }}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image
+              className="rounded-md"
+              src={"/image-4.jpg"}
+              // style={{ maxWidth: "100%", height: "auto" }}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image
+              className="rounded-md"
+              src={"/image-5.jpg"}
+              // style={{ maxWidth: "100%", height: "auto" }}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image
+              className="rounded-md"
+              src={"/image-6.jpg"}
+              // style={{ maxWidth: "100%", height: "auto" }}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image
+              className="rounded-md"
+              src={"/image-7.jpg"}
+              // style={{ maxWidth: "100%", height: "auto" }}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image
+              className="rounded-md"
+              src={"/image-8.jpg"}
+              // style={{ maxWidth: "100%", height: "auto" }}
+              fill
+              sizes="50vw"
+              alt=""
+            />
+          </SwiperSlide>
+        </Swiper>
       </main>
     </>
   );
